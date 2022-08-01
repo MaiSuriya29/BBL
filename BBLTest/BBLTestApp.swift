b3f472ce008b0571bbb9784e910d905fd77321da
suriya.m <suriya.m@ayodiacompany.com>
2022-08-01 23:24:56 +0700

    Initial Commit

---
+++ BBLTest/BBLTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBLTestApp.swift
+//  BBLTest
+//
+//  Created by Suriya on 1/8/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct BBLTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBLTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BBLTest
+//
+//  Created by Suriya on 1/8/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
